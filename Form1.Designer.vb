f01f1ee540e77d52fa50287901018f6e2aba134f
myhillman <marc.hillman1@bigpond.com>
2024-04-27 23:05:20 +1000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,80 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        MenuStrip1 = New MenuStrip()
+        DuplicateProgramFilesToolStripMenuItem = New ToolStripMenuItem()
+        ShortcutsToFToolStripMenuItem = New ToolStripMenuItem()
+        DuplicateProgramFilesToolStripMenuItem1 = New ToolStripMenuItem()
+        MenuStrip1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {DuplicateProgramFilesToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(800, 24)
+        MenuStrip1.TabIndex = 0
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' DuplicateProgramFilesToolStripMenuItem
+        ' 
+        DuplicateProgramFilesToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ShortcutsToFToolStripMenuItem, DuplicateProgramFilesToolStripMenuItem1})
+        DuplicateProgramFilesToolStripMenuItem.Name = "DuplicateProgramFilesToolStripMenuItem"
+        DuplicateProgramFilesToolStripMenuItem.Size = New Size(73, 20)
+        DuplicateProgramFilesToolStripMenuItem.Text = "File Check"
+        ' 
+        ' ShortcutsToFToolStripMenuItem
+        ' 
+        ShortcutsToFToolStripMenuItem.Name = "ShortcutsToFToolStripMenuItem"
+        ShortcutsToFToolStripMenuItem.Size = New Size(199, 22)
+        ShortcutsToFToolStripMenuItem.Text = "Shortcuts to F:"
+        ' 
+        ' DuplicateProgramFilesToolStripMenuItem1
+        ' 
+        DuplicateProgramFilesToolStripMenuItem1.Name = "DuplicateProgramFilesToolStripMenuItem1"
+        DuplicateProgramFilesToolStripMenuItem1.Size = New Size(199, 22)
+        DuplicateProgramFilesToolStripMenuItem1.Text = "Duplicate Program Files"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(MenuStrip1)
+        MainMenuStrip = MenuStrip1
+        Name = "Form1"
+        Text = "Form1"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents DuplicateProgramFilesToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ShortcutsToFToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DuplicateProgramFilesToolStripMenuItem1 As ToolStripMenuItem
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.IO
+
+Public Class Form1
+    Private Sub ShortcutsToFToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ShortcutsToFToolStripMenuItem.Click
+        ' Find any shortcuts on the desktop that refer to F:, so we can change them to D:
+        Dim Shortcuts = System.IO.Directory.GetFiles("C:\Users\User\Desktop", "*.lnk")
+        For Each shortcut In Shortcuts
+            Dim shell = CreateObject("WScript.Shell")
+            Dim path = shell.CreateShortcut(shortcut).TargetPath
+            If UCase(path(0)) = "F" Then Debug.WriteLine(path)
+        Next
+        Debug.WriteLine($"{Shortcuts.Count} shortcuts processed")
+    End Sub
+
+    Private Sub DuplicateProgramFilesToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles DuplicateProgramFilesToolStripMenuItem1.Click
+        ' Detect duplicate folders in D:\Program Files\ and D:\Program Files (x86). The x86 one is probably a leftover and can be removed.
+        Dim PFfolders As New List(Of String), PF86folders As New List(Of String)
+
+        Dim di As New DirectoryInfo("D:\Program Files\")
+        Dim PF = di.GetDirectories
+        For Each f In PF
+            PFfolders.Add(Strings.Replace(f.FullName, di.FullName, ""))
+        Next
+
+        di = New DirectoryInfo("D:\Program Files (x86)\")
+        Dim PF86 = di.GetDirectories
+        For Each f In PF86
+            PF86folders.Add(Strings.Replace(f.FullName, di.FullName, ""))
+        Next
+
+        Dim both = PFfolders.Intersect(PF86folders)
+        For Each D In both.ToList
+            Debug.WriteLine(D)
+        Next
+    End Sub
+End Class
